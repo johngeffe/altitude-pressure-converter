0647c2bd9432719be2892a53dd667ba077c4b168
John Geffe <jgeffe@gmail.com>
2016-07-26 21:51:07 -0700

    updated example for Tonic.

--- example.js
+++ example.js
@@ -3,6 +3,7 @@ var AltConverter = require('altitude-pressure-converter');
 var config = {
     temp: 59.9,
     altitude: 1000,
+    pressure: 97722,
     units: {
         temp: 'F',
         altitude: 'ft',
@@ -12,3 +13,4 @@ var config = {
 
 console.log(AltConverter.altitudeToPressure(config));
 // returns 97722.72887817245 Pascals
+console.log(AltConverter.pressureToAltitude(config));
